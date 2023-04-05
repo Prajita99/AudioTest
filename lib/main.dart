b35ea971772aae33f48d33c821d1d6617dd4d80d
Prajita99 <prajitaadhikari2001@gmail.com>
2023-04-05 10:36:21 +0545

    changes on sound tracks

--- lib/main.dart
+++ lib/main.dart
@@ -4,81 +4,59 @@ import 'package:audioplayers/audioplayers.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  
+  Future<void> playSound(int soundNumber) async {
+    final player = AudioPlayer();
+    // play(player, soundNumber);
+      await player.setSourceUrl('assets/note$soundNumber.wav'); // equivalent to setSource(UrlSource(url));
+        await player.setVolume(1);
+          await player.setPlaybackRate(0.5); // half speed
+            AudioPlayer.global.setGlobalAudioContext(AudioContextConfig(/*...*/).build());
+  }
+
+  Expanded buildKey({required Color backgroundColor, required int soundNumber}) {
+   return Expanded(
+      child: TextButton(
+        style: TextButton.styleFrom(
+          backgroundColor: backgroundColor
+        ),
+        onPressed: () {
+          playSound(soundNumber);
+        },
+        child: Text(
+          'Click Me',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20.0,
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
-  Widget build(BuildContext context,dynamic AsyncSnapshot) {
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              TextButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.red),
-                ),
-                onPressed: () {
-                final PlayerMode = AssetSource('note1.wav');
-              }, 
-              child: Text('Click Me'),
-              ),
-              TextButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.orange),
-                ),
-                onPressed: () {
-                final PlayerMode = AssetSource('note2.wav');
-              }, 
-              child: Text('Click Me'),
-              ),
-              TextButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.yellow),
-                ),
-                onPressed: () {
-                final PlayerMode = AssetSource('note3.wav');
-              }, 
-              child: Text('Click Me'),
-              ),
-              TextButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.green),
-                ),
-                onPressed: () {
-                final PlayerMode = AssetSource('note4.wav');
-              }, 
-              child: Text('Click Me'),
-              ),
-              TextButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.black),
-                ),
-                onPressed: () {
-                final PlayerMode = AssetSource('note5.wav');
-              }, 
-              child: Text('Click Me'),
-              ),
-              TextButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.cyan),
-                ),
-                onPressed: () {
-                final player = AudioPlayer();
-                  await player.setSource(AssetSource('note6.wav'));
-              }, 
-              child: Text('Click Me'),
-              ),
-              TextButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.blue),
-                ),
-                onPressed: () {
-                final PlayerMode = AssetSource('note7.wav');
-              }, 
-              child: Text('Click Me'),
-              ),
+              buildKey(backgroundColor: 
+            Colors.red, soundNumber: 1),
+              buildKey(backgroundColor: Colors.orange, soundNumber: 2),
+              buildKey(backgroundColor: Colors.yellow, soundNumber: 3),
+              buildKey(backgroundColor: Colors.green, soundNumber: 4),
+              buildKey(backgroundColor: Colors.pink, soundNumber: 5),
+              buildKey(backgroundColor: Colors.cyan, soundNumber: 6),
+              buildKey(backgroundColor: Colors.blue, soundNumber: 7),
             ],
           ),
         ),
       ),
     );
   }
+
+  void play(AudioPlayer player, int soundNumber) {}
 }
